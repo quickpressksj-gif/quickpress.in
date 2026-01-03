11b9aae4fb45c9fb0e1cf4d1328c05f160e2aaf2
quickpressksj-gif <quickpressksj@gmail.com>
2026-01-03 18:07:22 +0530

    Update index.js

--- frontend/frontend/pages/index.js
+++ frontend/frontend/pages/index.js
@@ -10,3 +10,21 @@ export default function Home() {
     </div>
   );
 }
+<div>
+  <h3>Categories</h3>
+
+  <button>Shirt</button>
+  <button>Pant</button>
+  <button>Saree</button>
+  <button>Woolen</button>
+  <button>Dry Clean</button>
+</div>
+<div>
+  <h3>Products</h3>
+
+  <div>
+    <p>Shirt Iron</p>
+    <p>₹10</p>
+    <button>ADD</button>
+  </div>
+</div>
