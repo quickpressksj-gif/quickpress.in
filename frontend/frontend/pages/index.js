54dfe8bbfd615bd980ddb710e2ab88d0d7ab1618
quickpressksj-gif <quickpressksj@gmail.com>
2026-01-03 18:03:50 +0530

    Update index.js

--- frontend/frontend/pages/index.js
+++ frontend/frontend/pages/index.js
@@ -1,1 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>QuickPress</h1>
+      <p>10-minute Express Ironing</p>
 
+      <button>Get Started</button>
+    </div>
+  );
+}
