150bd3bf5fd66def61cc0ae654dacd9fc59b223f
quickpressksj-gif <quickpressksj@gmail.com>
2026-01-03 18:04:50 +0530

    Update Header.js

--- frontend/frontend/components/Header.js
+++ frontend/frontend/components/Header.js
@@ -1,1 +1,11 @@
+export default function Header() {
+  return (
+    <header style={{ padding: "10px", background: "#FFD700" }}>
+      <h2>QuickPress</h2>
 
+      <button>Location</button>
+      <button>Login</button>
+      <button>Cart</button>
+    </header>
+  );
+}
